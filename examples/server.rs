79293794cf08523a58add68baa6a94ec7d84fb5c
fmhyxeee <1179570770@qq.com>
2022-03-23 23:01:30 +0800

    完成notify通知

--- examples/server.rs
+++ examples/server.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use async_prost::AsyncProstStream;
 use futures::{SinkExt, StreamExt};
-use kv::{CommandRequest, CommandResponse, MemTable, Service};
+use kv::{CommandRequest, CommandResponse, MemTable, ServiceInner, Service};
 use tokio::net::TcpListener;
 use tracing::info;
 
@@ -9,7 +9,7 @@ use tracing::info;
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let service = Service::new(MemTable::new());
+    let service: Service = ServiceInner::new(MemTable::new()).into();
 
     let addr = "127.0.0.1:9527";
     let listener = TcpListener::bind(addr).await?;
